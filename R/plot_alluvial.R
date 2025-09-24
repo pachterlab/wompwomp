95c8a5a04a54b71084ed2f580d04ad051b406079
Conrad Oakes <oakesconrad@gmail.com>
2025-09-23 17:15:04 -0700

    Properly adding coord_flip to functions imported from ggplot2

--- R/plot_alluvial.R
+++ R/plot_alluvial.R
@@ -5,7 +5,7 @@
 #' @name wompwomp
 #'
 #' @importFrom dplyr mutate select group_by summarise arrange desc ungroup slice n pull filter bind_rows across matches all_of add_count distinct
-#' @importFrom ggplot2 ggplot aes geom_text scale_fill_manual labs after_stat annotate theme_void theme element_text rel ggsave guides scale_color_manual scale_x_continuous element_blank
+#' @importFrom ggplot2 ggplot aes geom_text scale_fill_manual labs after_stat annotate theme_void theme element_text rel ggsave guides scale_color_manual scale_x_continuous element_blank coord_flip
 #' @importFrom ggalluvial geom_alluvium geom_stratum stat_stratum stat_alluvium
 #' @importFrom ggfittext geom_fit_text
 #' @importFrom ggforce gather_set_data
